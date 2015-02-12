a6502d4145450f081feaa841b5aeb2bafc78e5eb
acorbi <acorbi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 07:17:24 +0000

    Added v1.0.0beta1
    git-svn-id: https://plugins.svn.wordpress.org/wpckan/tags/1.0.0beta1@1087892 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wpckan_metabox_logic.js
@@ -0,0 +1,201 @@
+const DATASET_ID_ATTR = "wpckan-dataset-id";
+const DATASET_TITLE_ATTR = "wpckan-dataset-title";
+const DATASET_NAME_ATTR = "wpckan-dataset-name";
+const DATASET_GROUPS_ATTR = "wpckan-dataset-groups";
+const DATASET_NUM_RESOURCES_ATTR = "wpckan-dataset-num-resources";
+const DATASET_ORG_ATTR = "wpckan-dataset-org";
+const CKAN_API_URL = "wpckan-api-url"
+const CKAN_BASE_URL = "wpckan-base-url"
+
+var field;
+var datasetList;
+var addButton;
+
+var DEBUG = false;
+
+var datasets = [];
+
+jQuery( document ).ready(function() {
+  if (DEBUG) console.log("wpckan_metabox_logic.js document ready");
+
+  //Init div elements
+  field = jQuery('#wpckan_related_datasets_add_field');
+  datasetList = jQuery('#wpckan_related_datasets_list');
+  addButton = jQuery("#wpckan_related_datasets_add_button");
+
+  getFormValue();
+  listDatasets();
+
+  clearField();
+  addButton.addClass("disabled");
+
+  // Instantiate the Bloodhound suggestion engine
+  var suggestions = new Bloodhound({
+    datumTokenizer: function (datum) {
+      return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: field.attr(CKAN_API_URL) + '3/action/package_search?q=%QUERY',
+      filter: function (json) {
+        if (json.success){
+          return jQuery.map(json.result.results, function (dataset) {
+            return {
+              id: dataset["id"],
+              title: dataset["title"],
+              name: dataset["name"],
+              groups: JSON.stringify(dataset["groups"]),
+              num_resources: dataset["num_resources"],
+              owner_org: dataset["owner_org"]
+            };
+          });
+        }
+      }
+    },limit:20
+  });
+
+  // Initialize the Bloodhound suggestion engine
+  suggestions.initialize();
+
+  // Instantiate the Typeahead UI
+  jQuery('.typeahead').typeahead(null, {
+    hint: true,
+    minLength: 1,
+    highlight: true,
+    displayKey: 'title',
+    source: suggestions.ttAdapter()
+  }).on("typeahead:selected",function(event,item,dataset){
+    if (DEBUG) {
+      console.log(item["id"]);
+      console.log(item["title"]);
+      console.log(item["name"]);
+      console.log(item["groups"]);
+      console.log(item["num_resources"]);
+      console.log(item["owner_org"]);
+    }
+
+    jQuery(this).attr(DATASET_ID_ATTR,item["id"]);
+    jQuery(this).attr(DATASET_TITLE_ATTR,escape(item["title"]));
+    jQuery(this).attr(DATASET_NAME_ATTR,item["name"]);
+    jQuery(this).attr(DATASET_GROUPS_ATTR,item["groups"]);
+    jQuery(this).attr(DATASET_NUM_RESOURCES_ATTR,item["num_resources"]);
+    jQuery(this).attr(DATASET_ORG_ATTR,item["owner_org"]);
+    addButton.removeClass("disabled");
+  });
+
+  // TODO improve
+  jQuery('.delete').on("click",function(){
+    var dataset_id = jQuery(this).attr(DATASET_ID_ATTR);
+    removeDatasetWithIdForEntry(dataset_id,this);
+  });
+
+});
+
+function wpckan_related_dataset_metabox_on_input(){
+  if (DEBUG) console.log("wpckan_related_dataset_metabox_on_input");
+
+  addButton.addClass("disabled");
+}
+
+function wpckan_related_dataset_metabox_add(){
+  if (DEBUG) console.log("wpckan_related_dataset_metabox_add");
+
+  var dataset_id = field.attr(DATASET_ID_ATTR);
+  var dataset_title = field.attr(DATASET_TITLE_ATTR);
+  var dataset_url = field.attr(CKAN_BASE_URL) + "/dataset/" + field.attr(DATASET_NAME_ATTR);
+  var dataset_groups = field.attr(DATASET_GROUPS_ATTR);
+  var dataset_num_resources = field.attr(DATASET_NUM_RESOURCES_ATTR);
+  var dataset_org = field.attr(DATASET_ORG_ATTR);
+
+  var dataset = getDatasetWithId(dataset_id);
+  if (dataset){
+    clearField();
+    return;
+  }
+
+  addDataset(true,dataset_id,dataset_title,dataset_url,dataset_groups,dataset_num_resources,dataset_org);
+  clearField();
+}
+
+function listDatasets(){
+  for (index in datasets){
+    dataset = datasets[index];
+    addDataset(false,dataset["dataset_id"],dataset["dataset_title"],dataset["dataset_url"],dataset["dataset_groups"],dataset["dataset_num_resources"],dataset["dataset_org"]);
+  }
+}
+
+function getFormValue(){
+  var datasets_json = jQuery("#wpckan_add_related_datasets_datasets").val();
+  if (DEBUG) console.log("getFormValue "+ datasets_json);
+  if (datasets_json) datasets = JSON.parse(datasets_json);
+}
+
+function setFormValue(){
+  var datasets_json = JSON.stringify(datasets);
+  if (DEBUG) console.log("setFormValue "+ datasets_json);
+  jQuery("#wpckan_add_related_datasets_datasets").val(datasets_json);
+}
+
+function addDataset(save_in_array,dataset_id,dataset_title,dataset_url,dataset_groups,dataset_num_resources,dataset_org){
+
+  if (save_in_array){
+    datasets.push({"dataset_id": dataset_id, "dataset_title": dataset_title, "dataset_url": dataset_url, "dataset_groups": dataset_groups, "dataset_num_resources": dataset_num_resources, "dataset_org": dataset_org});
+    if (DEBUG) console.log("Added dataset with id: " + dataset_id + " datasets in array: "+ datasets.length);
+    setFormValue();
+  }
+
+  var entry = jQuery('<p><a target="_blank" href='+dataset_url+'>'+unescape(dataset_title)+' ('+dataset_num_resources+')</a>   </p>');
+  var del = jQuery('<a class="delete error" '+DATASET_ID_ATTR+'='+dataset_id+' href="#">Delete</a>');
+  // TODO improve
+  jQuery(del).on("click",function(){
+    var dataset_id = jQuery(this).attr(DATASET_ID_ATTR);
+    removeDatasetWithIdForEntry(dataset_id,this);
+  });
+  entry.append(del);
+  datasetList.append(entry);
+}
+
+//TODO optimize (using lodash)
+function removeDatasetWithIdForEntry(id,entry){
+  entry.parentNode.remove();  //use plain javascript here to get the parent
+  var datasetIndex = getDatasetIndexWithId(id);
+  if (datasetIndex){
+    if (DEBUG) console.log("removing " + id + " from datasets");
+    datasets.splice(datasetIndex,1);
+    setFormValue();
+    return;
+  }
+
+}
+
+//TODO optimize (using lodash)
+function getDatasetWithId(id){
+  for (index in datasets){
+    if (datasets[index]["dataset_id"] == id){
+      return datasets[index];
+    }
+  }
+  return null;
+}
+
+//TODO optimize (using lodash)
+function getDatasetIndexWithId(id){
+  for (index in datasets){
+    if (datasets[index]["dataset_id"] == id){
+      return index;
+    }
+  }
+  return null;
+}
+
+function clearField(){
+  field.val("");
+}
+
+function deserialize(s) {
+  return unescape(s);
+}
+
+function serialize(s) {
+  return escape(s);
+}
---
+++ utils/wpckan_utils.php
@@ -0,0 +1,396 @@
+<?php
+
+  define("FREQ_NEVER","0");
+  define("FREQ_POST_PUBLISHED","1");
+  define("FREQ_POST_SAVED","2");
+  define("FILTER_ALL","0");
+  define("FILTER_ONLY_WITH_RESOURCES","1");
+  define("DEFAULT_LOG","/tmp/wpckan.log");
+
+  use Analog\Analog;
+  use Analog\Handler;
+
+  function wpckan_edit_post_logic_dataset_metabox($post_ID){
+    wpckan_log("wpckan_edit_post_logic_datasets_metabox: " . $post_ID);
+
+    if ( ! isset( $_POST['wpckan_add_related_datasets_nonce'] ) )
+    return $post_ID;
+
+    $nonce = $_POST['wpckan_add_related_datasets_nonce'];
+
+    if ( ! wp_verify_nonce( $nonce, 'wpckan_add_related_datasets' ) )
+    return $post_ID;
+
+    $datasets_json = $_POST['wpckan_add_related_datasets_datasets'];
+
+    // Update the meta field.
+    update_post_meta( $post_ID, 'wpckan_related_datasets', $datasets_json );
+
+  }
+
+  function wpckan_edit_post_logic_archive_post_metabox($post_ID){
+    wpckan_log("wpckan_edit_post_logic_archive_post_metabox: " . $post_ID);
+
+    if ( ! isset( $_POST['wpckan_archive_post_nonce'] ) )
+    return $post_ID;
+
+    $nonce = $_POST['wpckan_archive_post_nonce'];
+
+    if ( ! wp_verify_nonce( $nonce, 'wpckan_archive_post' ) )
+    return $post_ID;
+
+    $archive_orga = $_POST['wpckan_archive_post_orga'];
+    $archive_group = $_POST['wpckan_archive_post_group'];
+    $archive_freq = $_POST['wpckan_archive_post_freq'];
+
+    update_post_meta( $post_ID, 'wpckan_archive_post_orga', $archive_orga );
+    update_post_meta( $post_ID, 'wpckan_archive_post_group', $archive_group );
+    update_post_meta( $post_ID, 'wpckan_archive_post_freq', $archive_freq );
+
+    if (wpckan_post_should_be_archived_on_save( $post_ID )){
+      $post = get_post($post_ID);
+      try{
+        wpckan_api_archive_post_as_dataset($post);
+      }catch(Exception $e){
+        wpckan_log($e->getMessage());
+      }
+    }
+
+  }
+
+  /*
+  * Shortcodes
+  */
+
+  function wpckan_show_related_datasets($atts) {
+    wpckan_log("wpckan_show_related_datasets " . print_r($atts,true));
+
+    $related_datasets_json = get_post_meta( $atts['post_id'], 'wpckan_related_datasets', true );
+    $related_datasets = array();
+    if (!IsNullOrEmptyString($related_datasets_json))
+      $related_datasets = json_decode($related_datasets_json,true);
+
+    $limit = 0;
+    if (array_key_exists("limit",$atts)){
+      $limit = (int)($atts["limit"]);
+    }
+
+    $page = 0;
+    if (array_key_exists("limit",$atts) && array_key_exists("page",$atts)){
+      $page = (int)($atts["page"]);
+    }
+
+    $filter = FILTER_ALL;
+    if (array_key_exists("filter",$atts)){
+      $filter = $atts["filter"];
+    }
+
+    $count = 0;
+    $dataset_array = array();
+    $atts["related_datasets"] = $related_datasets;
+    foreach ($related_datasets as $dataset){
+      if (($page == 0) || (($count >= (($page-1) * $limit)) && ($count <= ($page * $limit)))){
+        $dataset_atts = array("id" => $dataset["dataset_id"]);
+        try{
+          if ($filter == FILTER_ALL || (($filter == FILTER_ONLY_WITH_RESOURCES) && wpckan_dataset_has_resources($dataset))){
+           array_push($dataset_array,wpckan_api_get_dataset($dataset_atts));
+          }
+        }catch(Exception $e){
+          wpckan_log($e->getMessage());
+        }
+        if (($limit != 0) && (count($dataset_array) >= $limit)) break;
+      }
+      $count++;
+    }
+    return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_list.php',$dataset_array,$atts);
+  }
+
+  function wpckan_show_number_of_related_datasets($atts) {
+    wpckan_log("wpckan_show_number_of_related_datasets " . print_r($atts,true));
+
+    $related_datasets_json = get_post_meta( $atts['post_id'], 'wpckan_related_datasets', true );
+    $related_datasets = array();
+    if (!IsNullOrEmptyString($related_datasets_json))
+    $related_datasets = json_decode($related_datasets_json,true);
+
+    if (array_key_exists("group",$atts))
+      $filter_group = $atts["group"];
+    if (array_key_exists("organization",$atts)){
+      $filter_organization = $atts["organization"];
+    }
+
+    $filter = FILTER_ALL;
+    if (array_key_exists("filter",$atts)){
+      $filter = $atts["filter"];
+    }
+
+    $dataset_array = array();
+    foreach ($related_datasets as $dataset){
+
+      $qualifies = false;
+
+      if (!isset($filter_group) && !isset($filter_organization)){
+        $qualifies = true;
+      }else{
+
+        // Check if dataset belongs to group
+        if (isset($filter_group)){
+          $qualifies = false;
+          $groups = json_decode($dataset["dataset_groups"], true);
+          foreach ($groups as $group){
+            if (strtolower($filter_group) == strtolower($group["name"])){
+              $qualifies = true;
+            }
+          }
+        }
+
+        // Check if dataset belongs to organization
+        if (isset($filter_organization) && isset($dataset["dataset_org"])){
+          $qualifies = false;
+          try{
+            $organization = wpckan_api_get_organization($dataset["dataset_org"]);
+            if ( $organization["name"] == $filter_organization){
+              $qualifies = true;
+            }
+          }catch(Exception $e){
+            wpckan_log($e->getMessage());
+          }
+        }
+      }
+
+      if ($qualifies){
+        $dataset_atts = array("id" => $dataset["dataset_id"]);
+        try{
+         if ($filter == FILTER_ALL || (($filter == FILTER_ONLY_WITH_RESOURCES) && wpckan_dataset_has_resources($dataset))){
+          array_push($dataset_array,wpckan_api_get_dataset($dataset_atts));
+         }
+        }catch(Exception $e){
+          wpckan_log($e->getMessage());
+        }
+        if (array_key_exists("limit",$atts) && (count($dataset_array) >= (int)($atts["limit"]))) break;
+      }
+    }
+    return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_number.php',$dataset_array,$atts);
+  }
+
+  function wpckan_show_query_datasets($atts) {
+    wpckan_log("wpckan_show_query_datasets "  . print_r($atts,true));
+
+    $dataset_array = array();
+    try{
+      $result = wpckan_api_query_datasets($atts);
+      $dataset_array = $result["results"];
+      $atts["count"] = $result["count"];
+    }catch(Exception $e){
+      wpckan_log($e->getMessage());
+    }
+
+    $filter = FILTER_ALL;
+    if (array_key_exists("filter",$atts)){
+      $filter = $atts["filter"];
+    }
+
+    $filtered_dataset_array = array();
+    foreach ($dataset_array as $dataset){
+     if ($filter == FILTER_ALL || (($filter == FILTER_ONLY_WITH_RESOURCES) && wpckan_dataset_has_resources($dataset))){
+      array_push($filtered_dataset_array,wpckan_api_get_dataset($dataset));
+     }
+    }
+
+    return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_list.php',$filtered_dataset_array,$atts);
+  }
+
+  /*
+  * Templates
+  */
+
+  function wpckan_output_template($template_url,$data,$atts){
+    ob_start();
+    require $template_url;
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+  }
+
+  function wpckan_get_link_to_dataset($dataset_name){
+    wpckan_log("wpckan_get_link_to_dataset "  . print_r($dataset_name,true));
+
+    return get_option('setting_ckan_url') . "/dataset/" . $dataset_name;
+  }
+
+  function wpckan_get_link_to_resource($dataset_name,$resource_id){
+    wpckan_log("wpckan_get_link_to_resource "  . print_r($dataset_name,true) . " " . print_r($resource_id,true));
+
+    return wpckan_get_link_to_dataset($dataset_name) . "/resource/" . $resource_id;
+  }
+
+  /*
+  * Logging
+  */
+
+  function wpckan_log($text) {
+    if (!get_option('setting_log_enabled')) return;
+    if (!IsNullOrEmptyString(get_option('setting_log_path')))
+      Analog::handler(Handler\File::init (get_option('setting_log_path')));
+    else
+      Analog::handler(Handler\File::init (DEFAULT_LOG));
+
+    Analog::log ($text);
+  }
+
+  /*
+  * Utilities
+  */
+
+  function wpckan_is_supported_post_type($post_type){
+   $settings_name =  "setting_supported_post_types_" . $post_type;
+   return get_option($settings_name);
+  }
+
+  function wpckan_dataset_has_resources($dataset){
+    if (array_key_exists("dataset_num_resources",$dataset)){
+     return ($dataset["dataset_num_resources"] >= 1);
+    }
+
+    if (array_key_exists("num_resources",$dataset)){
+     return ($dataset["num_resources"] >= 1);
+    }
+    return false;
+  }
+
+  function wpckan_cleanup_text_for_archiving($post_content){
+    $post_content = wpckan_detect_and_remove_shortcodes_in_text($post_content);
+    return $post_content;
+  }
+
+  function wpckan_detect_and_remove_shortcodes_in_text($text)
+  {
+    global $post;
+    $pattern = get_shortcode_regex();
+    $shortcodes = array("wpckan_related_datasets","wpckan_number_of_related_datasets","wpckan_query_datasets");
+
+    foreach($shortcodes as $shortcode){
+      if (   preg_match_all( '/'. $pattern .'/s', $post->post_content, $matches )
+      && array_key_exists( 2, $matches )
+      && in_array( $shortcode, $matches[2] ) )
+      {
+        foreach($matches as $match){
+          $text = str_replace($match,"",$text);
+        }
+      }
+    }
+    return $text;
+  }
+
+  function wpckan_detect_and_echo_shortcodes_in_text($text)
+  {
+    global $post;
+    $pattern = get_shortcode_regex();
+    $shortcodes = array("wpckan_related_datasets","wpckan_number_of_related_datasets","wpckan_query_datasets");
+
+    foreach($shortcodes as $shortcode){
+      if (   preg_match_all( '/'. $pattern .'/s', $post->post_content, $matches )
+      && array_key_exists( 2, $matches )
+      && in_array( $shortcode, $matches[2] ) )
+      {
+        foreach($matches as $match){
+          $text = str_replace($match,do_shortcode($match),$text);
+        }
+      }
+    }
+    return $text;
+  }
+
+  function wpckan_get_complete_url_for_dataset($dataset){
+    return get_option('setting_ckan_url') . "/dataset/" . $dataset["name"];
+  }
+
+  function wpckan_get_group_names_for_user(){
+    $groups = array();
+    $group_names = array();
+    try{
+      $groups = wpckan_api_get_group_list_for_user();
+    }catch(Exception $e){
+      wpckan_log($e->getMessage());
+    }
+    foreach ($groups as $group){
+      array_push($group_names,$group["display_name"]);
+    }
+    return $group_names;
+  }
+
+  function wpckan_get_organization_names_for_user(){
+    $organizations = array();
+    $organization_names = array();
+    try{
+      $organizations = wpckan_api_get_organization_list_for_user();
+    }catch(Exception $e){
+      wpckan_log($e->getMessage());
+    }
+    foreach ($organizations as $organization){
+      array_push($organization_names,$organization["display_name"]);
+    }
+    return $organization_names;
+  }
+
+  function wpckan_validate_settings_read(){
+    try{
+      wpckan_api_ping();
+    }catch(Exception $e){
+      wpckan_log($e->getMessage());
+      return false;
+    }
+    return true;
+  }
+
+  function wpckan_validate_settings_write(){
+    return !IsNullOrEmptyString(get_option('setting_ckan_api'));
+  }
+
+  function wpckan_get_ckan_settings(){
+
+      $settings = array(
+        'baseUrl' => get_option('setting_ckan_url') . "/api/",
+        'scheme' => 'http',
+        'apiKey' => get_option('setting_ckan_api')
+      );
+
+      return $settings;
+    }
+
+  function wpckan_post_should_be_archived_on_publish($post_ID){
+    $archive_freq = get_post_meta( $post_ID, 'wpckan_archive_post_freq', true);
+    wpckan_log("wpckan_post_should_be_archived_on_publish freq: " . $archive_freq);
+    return ( $archive_freq == FREQ_POST_PUBLISHED);
+  }
+
+  function wpckan_post_should_be_archived_on_save($post_ID){
+    $archive_freq = get_post_meta( $post_ID, 'wpckan_archive_post_freq', true);
+    wpckan_log("wpckan_post_should_be_archived_on_save freq: " . $archive_freq);
+    return ( $archive_freq == FREQ_POST_SAVED);
+  }
+
+  function wpckan_sanitize_url($input) {
+    $clean_url = esc_url($input);
+    if(substr($clean_url, -1) == '/') {
+      $clean_url = substr($clean_url, 0, -1);
+    }
+    return $clean_url;
+  }
+
+  function wpckan_pagination_last($count,$limit,$page) {
+    wpckan_log("wpckan_pagination_last");
+    return (($count >= ($limit * ($page -1 ))) && ($count <= ($limit * $page)));
+  }
+
+  function wpckan_pagination_first($page) {
+    wpckan_log("wpckan_pagination_first");
+    return ($page == 1);
+  }
+
+  function IsNullOrEmptyString($question){
+    return (!isset($question) || trim($question)==='');
+  }
+
+
+?>
